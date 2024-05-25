a72dbaa3feb49bc31f21507ef4f591d901396582
Ilya Vlasyuk <vlasuyk.2015.1@mail.ru>
2024-05-26 02:16:09 +0300

    Initial commit

---
+++ run-grafana.sh
@@ -0,0 +1,1 @@
+docker run --name mygrafana -d -p 3000:3000 grafana/grafana
---
+++ run-prometheus.sh
@@ -0,0 +1,1 @@
+docker run -d --name myprometheus -p 9090:9090 -v /home/andrew/GeekBrains/HomeWorks/spring-11/homework5/src/main/resources/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
